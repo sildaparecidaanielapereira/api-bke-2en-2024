486f3ea8742850f6966f50fd8a4993c05ee1564d
sildaparecidaanielapereira <julipantera2@gmail.com>
2024-08-13 16:49:22 -0300

    validação rotas

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log ('olá mundo!')
+import express from 'express'
+import authRouter from './routers/authRouter.js'
+import accountRouter from './routers/accountRouter.js'
+
+const app = express()
+
+app.use('/auth', authRouter)
+app.use('/account', accountRouter)
+
+app.listen(3000, () => {
+    console.log('Servidor Rodando em http://localhost:3000')
+})
