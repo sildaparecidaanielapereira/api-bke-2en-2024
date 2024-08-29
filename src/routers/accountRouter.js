6873b87b01f5e3e962cadaa47f44f9e54a780fc5
sildaparecidaanielapereira <julipantera2@gmail.com>
2024-08-29 17:43:53 -0300

    tudo ok

--- src/routers/accountRouter.js
+++ src/routers/accountRouter.js
@@ -1,21 +1,16 @@
 import express from 'express'
+import createController from '../../account/createController.js'
+import getByIdController from '../../account/getByIdController.js'
+import listController from '../../account/listController.js'
+import updateController from '../../account/updateController.js'
+import removeController from '../../account/removeController.js'
 
 const router = express.Router()
 
-router.post('/', (req, res) => {
-    res.json({message: "Rota de POST Account"})
-})
-router.get('/list', (req, res) => {
-    res.json({message: "Rota de GET Account/List"})
-})
-router.get('/:id', (req, res) => {
-    res.json({message: "Rota de GET Account ID"})
-})
-router.put('/:id', (req, res) => {
-    res.json({message: "Rota de PUT Account"})
-})
-router.delete('/:id', (req, res) => {
-    res.json({message: "Rota de DELETE Account"})
-})
+router.post('/', createController)
+router.get('/list', listController)
+router.get('/:id', getByIdController)
+router.put('/:id', updateController)
+router.delete('/:id', removeController)
 
 export default router
