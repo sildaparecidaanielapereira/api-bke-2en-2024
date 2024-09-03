23563a1e5cb254831a2f6ec6af85d116e361ab6f
sildaparecidaanielapereira <julipantera2@gmail.com>
2024-09-03 17:16:30 -0300

    tudo serto ne

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ import express from 'express'
 import authRouter from './routers/authRouter.js'
 import accountRouter from './routers/accountRouter.js'
 import { ENVIRONMENT, PORT, HOST } from './config.js'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {res.json({message: "Bem-vindo a API!"})})
 
